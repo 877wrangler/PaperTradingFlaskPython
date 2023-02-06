fc34bd5e568015eef2e3a8f06b9e8cceba2d3bd8
877wrangler <877wrangler@gmail.com>
2023-02-06 15:25:08 -0500

    Create FlaskPythonPaperTrading App

---
+++ app.py
@@ -0,0 +1,48 @@
+import requests
+import finnhub
+from flask import Flask, render_template, session, request, jsonify
+from flask_session import Session
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+finnhub_client = finnhub.Client(api_key="cfgldq1r01qlga2ugkvgcfgldq1r01qlga2ugl00")
+
+@app.route("/")
+# @login_required
+def index():
+    # user_id = session["user_id"]
+    # print(user_id)
+    # account_info = db.execute("SELECT * FROM account WHERE user_id LIKE ?", (user_id))
+    # print(account_info)
+    # data = []
+    # for row in account_info:
+    #     data.append(row)
+    #     print(row)
+    # print(data)
+    return render_template("index.html")
+
+
+@app.route("/quote", methods=["GET", "POST"])
+# @login_required
+def quote():
+    """Get stock quote."""
+    ticker = request.form.get("quote")
+    latest_price = None
+    if request.method == "POST":
+        quote = finnhub_client.quote(ticker)
+        latest_price = quote['c']
+        print(latest_price)
+
+    return render_template("quote.html", latest_price=latest_price, ticker=ticker)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
