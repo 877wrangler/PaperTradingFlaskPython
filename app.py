e601d58a0d7d8b74eaa1c6396ef35df296ee61e6
877wrangler <877wrangler@gmail.com>
2023-02-06 16:54:41 -0500

    Implement Jinja

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import requests
 import finnhub
-from flask import Flask, render_template, session, request, jsonify
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 from flask_session import Session
 import os
+import sqlite3
 from dotenv import load_dotenv
 
+# Get API
 dotenv_path = "C:/Users/Randy/PycharmProjects/FlashPythonPaperTrading/api.env"
 load_dotenv(dotenv_path)
 api_key = os.getenv("API_KEY")
-print(api_key)
+finnhub_client = finnhub.Client(api_key=api_key)
+
 # Configure application
 app = Flask(__name__)
 
@@ -17,7 +22,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-finnhub_client = finnhub.Client(api_key=api_key)
+# Open database
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
 
 @app.route("/")
 # @login_required
@@ -32,6 +39,26 @@ def index():
     #     print(row)
     # print(data)
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    register_username = request.form.get("register_username")
+    register_password = request.form.get("register_password")
+
+    if request.method == "POST":
+        # Check if the username exists
+        username_exists = cursor.execute("SELECT username FROM users WHERE username = ?", (register_username))
+
+        # If the username doesn't exist, hash the password and insert the data
+        if not username_exists:
+            password_hash = generate_password_hash(register_password)
+            print(password_hash)
+            cursor.execute("INSERT INTO users (username, hash, cash) VALUES (?, ?, ?)", register_username, password_hash, 10000)
+            conn.commit()
+            return redirect("/login")
+
+    return render_template("register.html")
 
 
 @app.route("/quote", methods=["GET", "POST"])
